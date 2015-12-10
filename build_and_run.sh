c481c9fdf4a5aa143893f197aeea5ab1ce519fe2
Bradley J Chambers <brad.chambers@gmail.com>
2015-12-10 11:53:49 -0500

    Split into three scripts for convenience

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,3 @@
 docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.4.2 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hello'
 docker build -t venicegeo/pdal-microservice .
+docker run --rm -it -p 8080:8080 -v ~/.aws/credentials:/root/.aws/credentials venicegeo/pdal-microservice
