55e8e28c370f0324ab71d121e1bf24c917ad0359
Bradley J Chambers <brad.chambers@gmail.com>
2015-12-14 12:20:28 -0500

    Add some scripts for running tests

---
+++ run-s3-info.sh
@@ -0,0 +1,1 @@
+curl -v -X POST -H "Content-Type: application/json" -d '{"source":{"bucket":"venicegeo-sample-data","key":"pointcloud/samp71-utm.laz"},"function":"info"}' http://192.168.99.100:8080/pdal
