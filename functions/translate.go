9e4059516e673d82fdca532a4b4807390a62b977
Bradley J Chambers <brad.chambers@gmail.com>
2016-01-11 12:37:12 -0500

    Fix link

--- functions/translate.go
+++ functions/translate.go
@@ -35,7 +35,7 @@ Assume the following command:
 
 The Translate function takes a single option "args" - a string that completes the command-line invocation of the PDAL CLI.
 
-See [here](http://www.pdal.io/apps.html#translate-command) for more on the PDAL translate command.
+See http://www.pdal.io/apps.html#translate-command for more on the PDAL translate command.
 */
 type TranslateOptions struct {
 	Args string `json:"args"`
