647af791ac0bc6d5da81632f68545240d65a93bc
Bradley J Chambers <brad.chambers@gmail.com>
2016-01-28 13:01:36 -0500

    debugging

--- handlers/pdal.go
+++ handlers/pdal.go
@@ -17,6 +17,7 @@ limitations under the License.
 package handlers
 
 import (
+	"log"
 	"net/http"
 	"time"
 
@@ -76,7 +77,9 @@ func PdalHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		utils.MakeFunction(functions.Translate)(w, r, &res, msg)
 
 	case "vo":
+		log.Println("About to call MakeFunction from PdalHandler")
 		utils.MakeFunction(functions.VO)(w, r, &res, msg)
+		log.Println("Returned from MakeFunction")
 
 	// An unrecognized function will result in 400 error, with message explaining
 	// how to list available functions.
