c1d110eb7b7804268cd1a973b00a69da92571f31
Bradley J Chambers <brad.chambers@gmail.com>
2016-02-02 14:54:29 -0500

    Dummy commit to force Jenkins

--- app.go
+++ app.go
@@ -176,6 +176,7 @@ func main() {
 		defaultPort = "8080"
 	}
 
+	log.Println("Force Jenkins to rebuild")
 	log.Println("Starting on port ", defaultPort)
 	if err := http.ListenAndServe(":"+defaultPort, router); err != nil {
 		log.Fatal(err)
