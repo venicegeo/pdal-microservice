c2bbbbc939d00d1f8f9c348af857eac7bd4e7a2c
Bradley J Chambers <brad.chambers@gmail.com>
2016-02-03 10:42:38 -0500

    Whittle down to only those items that really need to be tested here

--- handlers/pdal_test.go
+++ handlers/pdal_test.go
@@ -26,78 +26,80 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func TestBasicInfo(t *testing.T) {
-	userJSON := `{
-		"source":
-		{
-			"bucket": "venicegeo-sample-data",
-			"key": "pointcloud/samp71-utm.laz"
-		},
-		"function": "info"
-	}`
-	reader := strings.NewReader(userJSON)
-	router := httprouter.New()
-	router.POST("/pdal", PdalHandler)
-	req, _ := http.NewRequest("POST", "/pdal", reader)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	fmt.Println(w.Body)
-	if w.Code != http.StatusOK {
-		t.Errorf("StatusOK expected: %d", w.Code)
-	}
-}
+// these require PDAL - not appropriate for Jenkins
+// func TestBasicInfo(t *testing.T) {
+// 	userJSON := `{
+// 		"source":
+// 		{
+// 			"bucket": "venicegeo-sample-data",
+// 			"key": "pointcloud/samp71-utm.laz"
+// 		},
+// 		"function": "info"
+// 	}`
+// 	reader := strings.NewReader(userJSON)
+// 	router := httprouter.New()
+// 	router.POST("/pdal", PdalHandler)
+// 	req, _ := http.NewRequest("POST", "/pdal", reader)
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	fmt.Println(w.Body)
+// 	if w.Code != http.StatusOK {
+// 		t.Errorf("StatusOK expected: %d", w.Code)
+// 	}
+// }
+//
+// func TestBasicGround(t *testing.T) {
+// 	userJSON := `{
+// 		"source":
+// 		{
+// 			"bucket": "venicegeo-sample-data",
+// 			"key": "pointcloud/samp71-utm.laz"
+// 		},
+// 		"function": "ground",
+// 		"destination":
+// 		{
+// 			"bucket": "venicegeo-sample-data",
+// 			"key": "temp/ground.laz"
+// 		}
+// 	}`
+// 	reader := strings.NewReader(userJSON)
+// 	router := httprouter.New()
+// 	router.POST("/pdal", PdalHandler)
+// 	req, _ := http.NewRequest("POST", "/pdal", reader)
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	fmt.Println(w.Body)
+// 	if w.Code != http.StatusOK {
+// 		t.Errorf("StatusOK expected: %d", w.Code)
+// 	}
+// }
+//
+// func TestBasicHeight(t *testing.T) {
+// 	userJSON := `{
+// 		"source":
+// 		{
+// 			"bucket": "venicegeo-sample-data",
+// 			"key": "pointcloud/samp71-utm.laz"
+// 		},
+// 		"function": "height",
+// 		"destination": {
+// 			"bucket": "venicegeo-sample-data",
+// 			"key": "temp/height.laz"
+// 		}
+// 	}`
+// 	reader := strings.NewReader(userJSON)
+// 	router := httprouter.New()
+// 	router.POST("/pdal", PdalHandler)
+// 	req, _ := http.NewRequest("POST", "/pdal", reader)
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	fmt.Println(w.Body)
+// 	if w.Code != http.StatusOK {
+// 		t.Errorf("StatusOK expected: %d", w.Code)
+// 	}
+// }
 
-func TestBasicGround(t *testing.T) {
-	userJSON := `{
-		"source":
-		{
-			"bucket": "venicegeo-sample-data",
-			"key": "pointcloud/samp71-utm.laz"
-		},
-		"function": "ground",
-		"destination":
-		{
-			"bucket": "venicegeo-sample-data",
-			"key": "temp/ground.laz"
-		}
-	}`
-	reader := strings.NewReader(userJSON)
-	router := httprouter.New()
-	router.POST("/pdal", PdalHandler)
-	req, _ := http.NewRequest("POST", "/pdal", reader)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	fmt.Println(w.Body)
-	if w.Code != http.StatusOK {
-		t.Errorf("StatusOK expected: %d", w.Code)
-	}
-}
-
-func TestBasicHeight(t *testing.T) {
-	userJSON := `{
-		"source":
-		{
-			"bucket": "venicegeo-sample-data",
-			"key": "pointcloud/samp71-utm.laz"
-		},
-		"function": "height",
-		"destination": {
-			"bucket": "venicegeo-sample-data",
-			"key": "temp/height.laz"
-		}
-	}`
-	reader := strings.NewReader(userJSON)
-	router := httprouter.New()
-	router.POST("/pdal", PdalHandler)
-	req, _ := http.NewRequest("POST", "/pdal", reader)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	fmt.Println(w.Body)
-	if w.Code != http.StatusOK {
-		t.Errorf("StatusOK expected: %d", w.Code)
-	}
-}
-
+// this is specific to our service, it stays
 func TestNoFunctionField(t *testing.T) {
 	userJSON := `{
 		"source":
@@ -119,6 +121,7 @@ func TestNoFunctionField(t *testing.T) {
 	}
 }
 
+// also specific to our service, it too stays
 func TestBadFunction(t *testing.T) {
 	userJSON := `{
 		"source":
@@ -140,73 +143,74 @@ func TestBadFunction(t *testing.T) {
 	}
 }
 
-func TestBadBucket(t *testing.T) {
-	userJSON := `{
-		"source":
-		{
-			"bucket": "bad-bucket",
-			"key": "pointcloud/samp71-utm.laz"
-		},
-		"function": "info"
-	}`
-	reader := strings.NewReader(userJSON)
-	router := httprouter.New()
-	router.POST("/pdal", PdalHandler)
-	req, _ := http.NewRequest("POST", "/pdal", reader)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	fmt.Println(w.Body)
-	if w.Code != http.StatusInternalServerError {
-		t.Errorf("StatusInternalServerError expected: %d", w.Code)
-	}
-}
-
-func TestBadKey(t *testing.T) {
-	userJSON := `{
-		"source":
-		{
-			"bucket": "venicegeo-sample-data",
-			"key": "bad-folder/bad-file"
-		},
-		"function": "info"
-	}`
-	reader := strings.NewReader(userJSON)
-	router := httprouter.New()
-	router.POST("/pdal", PdalHandler)
-	req, _ := http.NewRequest("POST", "/pdal", reader)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	fmt.Println(w.Body)
-	if w.Code != http.StatusInternalServerError {
-		t.Errorf("StatusInternalServerError expected: %d", w.Code)
-	}
-}
-
-func TestEmptyJSON(t *testing.T) {
-	userJSON := `{}`
-	reader := strings.NewReader(userJSON)
-	router := httprouter.New()
-	router.POST("/pdal", PdalHandler)
-	req, _ := http.NewRequest("POST", "/pdal", reader)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	fmt.Println(w.Body)
-	if w.Code != http.StatusBadRequest {
-		t.Errorf("StatusBadRequest expected: %d", w.Code)
-	}
-}
-
-func TestNoJSON(t *testing.T) {
-	router := httprouter.New()
-	router.POST("/pdal", PdalHandler)
-	req, _ := http.NewRequest("POST", "/pdal", nil)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	fmt.Println(w.Body)
-	if w.Code != http.StatusBadRequest {
-		t.Errorf("StatusBadRequest expected: %d", w.Code)
-	}
-}
+// these really test elements of pzsvc-sdk-go
+// func TestBadBucket(t *testing.T) {
+// 	userJSON := `{
+// 		"source":
+// 		{
+// 			"bucket": "bad-bucket",
+// 			"key": "pointcloud/samp71-utm.laz"
+// 		},
+// 		"function": "info"
+// 	}`
+// 	reader := strings.NewReader(userJSON)
+// 	router := httprouter.New()
+// 	router.POST("/pdal", PdalHandler)
+// 	req, _ := http.NewRequest("POST", "/pdal", reader)
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	fmt.Println(w.Body)
+// 	if w.Code != http.StatusInternalServerError {
+// 		t.Errorf("StatusInternalServerError expected: %d", w.Code)
+// 	}
+// }
+//
+// func TestBadKey(t *testing.T) {
+// 	userJSON := `{
+// 		"source":
+// 		{
+// 			"bucket": "venicegeo-sample-data",
+// 			"key": "bad-folder/bad-file"
+// 		},
+// 		"function": "info"
+// 	}`
+// 	reader := strings.NewReader(userJSON)
+// 	router := httprouter.New()
+// 	router.POST("/pdal", PdalHandler)
+// 	req, _ := http.NewRequest("POST", "/pdal", reader)
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	fmt.Println(w.Body)
+// 	if w.Code != http.StatusInternalServerError {
+// 		t.Errorf("StatusInternalServerError expected: %d", w.Code)
+// 	}
+// }
+//
+// func TestEmptyJSON(t *testing.T) {
+// 	userJSON := `{}`
+// 	reader := strings.NewReader(userJSON)
+// 	router := httprouter.New()
+// 	router.POST("/pdal", PdalHandler)
+// 	req, _ := http.NewRequest("POST", "/pdal", reader)
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	fmt.Println(w.Body)
+// 	if w.Code != http.StatusBadRequest {
+// 		t.Errorf("StatusBadRequest expected: %d", w.Code)
+// 	}
+// }
+//
+// func TestNoJSON(t *testing.T) {
+// 	router := httprouter.New()
+// 	router.POST("/pdal", PdalHandler)
+// 	req, _ := http.NewRequest("POST", "/pdal", nil)
+// 	w := httptest.NewRecorder()
+// 	router.ServeHTTP(w, req)
+// 	fmt.Println(w.Body)
+// 	if w.Code != http.StatusBadRequest {
+// 		t.Errorf("StatusBadRequest expected: %d", w.Code)
+// 	}
+// }
 
 func TestBadEndpoint(t *testing.T) {
 	router := httprouter.New()
