266b2a4098627569491f40630cf7946105c9f0ad
Bradley J Chambers <brad.chambers@gmail.com>
2016-05-16 12:37:51 -0400

    Add dedicated VO handler

---
+++ handlers/vo.go
@@ -0,0 +1,127 @@
+/*
+Copyright 2016, RadiantBlue Technologies, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+// VoOptions defines options for the VO function.
+type VoOptions struct {
+	Filename    *string `json:"filename"` // list of input file paths
+	AGL         float64 `json:"agl"`
+	MaxSize     uint16  `json:"max_size"`
+	MinSize     uint16  `json:"min_size"`
+	Resolution  float64 `json:"resolution"`
+	Tolerance   float64 `json:"tolerance"`
+	Z0Tolerance float64 `json:"z0_tolerance"`
+}
+
+// NewVoOptions constructs VoOptions with default values.
+func NewVoOptions() *VoOptions {
+	return &VoOptions{AGL: 20.0, MaxSize: 65535, MinSize: 1, Resolution: 10, Tolerance: 3, Z0Tolerance: 6.0}
+}
+
+func getFloatAsString(name string, val float64) string {
+	return name + "=" + strconv.FormatFloat(val, 'f', -1, 64)
+}
+
+func getUintAsString(name string, val uint16) string {
+	return name + "=" + strconv.FormatUint(uint64(val), 10)
+}
+
+// VoHandler handles PDAL jobs.
+func VoHandler(w http.ResponseWriter, r *http.Request) *AppError {
+	if r.Body == nil {
+		return &AppError{nil, "No JSON", http.StatusBadRequest}
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return &AppError{err, err.Error(), http.StatusInternalServerError}
+	}
+
+	opts := NewVoOptions()
+	if b != nil {
+		if err := json.Unmarshal(b, &opts); err != nil {
+			return &AppError{err, err.Error(), http.StatusInternalServerError}
+		}
+	}
+
+	if opts.Filename == nil {
+		return &AppError{err, "No filename", http.StatusInternalServerError}
+	}
+
+	s3url, err := url.Parse(*opts.Filename)
+	if err != nil {
+		return &AppError{err, err.Error(), http.StatusInternalServerError}
+	}
+
+	name, _, err := inferAndDownload(s3url)
+	if err != nil {
+		return &AppError{err, err.Error(), http.StatusInternalServerError}
+	}
+
+	args := []string{
+		"translate", name, "out.json",
+		"-w", "writers.vo", "-v", "3", "--debug",
+		getFloatAsString("--writers.vo.agl", opts.AGL),
+		getUintAsString("--writers.vo.max_size", opts.MaxSize),
+		getUintAsString("--writers.vo.min_size", opts.MinSize),
+		getFloatAsString("--writers.vo.resolution", opts.Resolution),
+		getFloatAsString("--writers.vo.tolerance", opts.Tolerance),
+		getFloatAsString("--writers.vo.z0_tolerance", opts.Z0Tolerance),
+	}
+	log.Println("PDAL CLI called with args", args)
+	out, err := exec.Command("pdal", args...).CombinedOutput()
+	if err != nil {
+		return &AppError{err, err.Error(), http.StatusInternalServerError}
+	}
+	log.Println("PDAL CLI responded with")
+	log.Println(string(out))
+
+	fileOut, err := os.Open("out.json")
+	if err != nil {
+		return &AppError{err, err.Error(), http.StatusInternalServerError}
+	}
+	defer fileOut.Close()
+
+	src, err := ioutil.ReadAll(fileOut)
+	if err != nil {
+		return &AppError{err, err.Error(), http.StatusInternalServerError}
+	}
+
+	buffer := new(bytes.Buffer)
+	if err := json.Compact(buffer, src); err != nil {
+		return &AppError{err, err.Error(), http.StatusInternalServerError}
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, buffer.String())
+
+	return nil
+}
